78c7ef6029c67a3c44c6accf3773e3c792a68b88
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2023-05-08 18:46:53 -0300

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ import Cards from '../components/Cards.jsx';
 import About from '../components/About';
 import Ciudad from '../components/Ciudad';
 import { Route, Switch } from 'react-router-dom';
-const apiKey = process.env.apikey;
+const apiKey = process.env.REACT_APP_APIKEY;
 //const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
 
 function App() {
